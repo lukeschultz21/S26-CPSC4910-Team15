58476f82e65f309893c4f906f6638c53c8e27b02
scottshaffer997 <143344076+scottshaffer997@users.noreply.github.com>
2026-02-03 01:25:23 +0000

    Created tables and initial attributes

--- database/schema.sql
+++ database/schema.sql
@@ -5,6 +5,54 @@ DROP SCHEMA IF EXISTS Team15_DB;
 CREATE SCHEMA Team15_DB;
 USE Team15_DB;
 
+CREATE TABLE USERS (
+    user_id INT AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE SPONSORORGANIZATION (
+    sponsorOrganization_id INT AUTO_INCREMENT PRIMARY KEY,
+    sponsor_name VARCHAR(100) NOT NULL UNIQUE
+);
+
+# Subtype of USERS
+CREATE TABLE DRIVERS (
+    user_id INT PRIMARY KEY,
+    sponsorOrganization_id INT NOT NULL,
+    points_earned INT DEFAULT 0,
+    account_status INT DEFAULT 1,  # 1 for active, 0 for inactive,
+
+    FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+    FOREIGN KEY (sponsorOrganization_id) REFERENCES SPONSORORGANIZATION(sponsorOrganization_id)
+);
+
+# Subtype of USERS
+CREATE TABLE SPONSORUSERS (
+    user_id INT PRIMARY KEY,
+    sponsorOrganization_id INT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+    FOREIGN KEY (sponsorOrganization_id) REFERENCES SPONSORORGANIZATION(sponsorOrganization_id)
+);
+
+# Subtype of USERS
+CREATE TABLE ADMIN (
+    user_id INT PRIMARY KEY,
+
+    FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+);
+
+CREATE TABLE DRIVERAPPLICATIONS (
+    application_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    sponsorOrganization_id INT NOT NULL,
+    application_status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    reasoning VARCHAR(300),
+    application_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+    FOREIGN KEY (sponsorOrganization_id) REFERENCES SPONSORORGANIZATION(sponsorOrganization_id)
+);
+
 
 
 
