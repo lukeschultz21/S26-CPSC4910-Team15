bf5f3a7aab18e4f2dbdcfb80476a273afbe5dd4e
Lernonad <mgrockow@icloud.com>
2026-02-12 14:18:59 -0500

    Update StoredProcedures.sql

--- database/StoredProcedures.sql
+++ database/StoredProcedures.sql
@@ -151,10 +151,6 @@ BEGIN
   END IF;
 END$$
 
-# CREATE TRIGGER audit_log_login_attempt
-  -- triggers when password database is checked
-# END$$
-
 CREATE TRIGGER audit_log_points_change 
 AFTER UPDATE ON POINTTRANSACTIONS
 BEGIN
@@ -168,27 +164,6 @@ BEGIN
     CONCAT(NEW.transaction_id, ' ', NEW.reason) -- notes
   );
 END$$
-
-CREATE TRIGGER audit_log_driver_application_made 
-AFTER INSERT ON DRIVERAPPLICATIONS 
-BEGIN
-  -- triggers when driver application is made
-  INSERT INTO AUDITLOG (action_type, actor_user_id, actee_user_id, org_id, notes)
-  VALUES (
-    'DRIVERAPPLICATION ADDED'
-    NEW.actor_user_id,
-    NEW.user_id,
-    NEW.org_id,
-    NEW.application_id
-  )
-END$$
-
-#CREATE TRIGGER audit_log_driver_application_status change 
-  -- triggers when driver application status is changed
-#END$$
-
-
-
 
 DELIMITER ;
 # Logs all login attempts to the AUDITLOG
